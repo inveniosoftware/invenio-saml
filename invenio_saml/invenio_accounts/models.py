23b7881a6ae1004b60fd97ab94a717df6c358788
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2019-05-03 10:56:48 -0400

    models: conditionally register UserIdentity

--- invenio_saml/invenio_accounts/models.py
+++ invenio_saml/invenio_accounts/models.py
@@ -6,25 +6,49 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """Temporary file for, IMHO, Invenio-Accounts code."""
-
+from invenio_accounts import __version__ as ACCOUNTS_VERSION
 from invenio_accounts.models import User
 from invenio_db import db
+from pkg_resources import DistributionNotFound, get_distribution, parse_version
 from sqlalchemy_utils import Timestamp
 
+HAS_USER_IDENTITY = True
+if parse_version(ACCOUNTS_VERSION) < parse_version('1.2.0'):
+    HAS_USER_IDENTITY = False
 
-class UserIdentity(db.Model, Timestamp):
-    """Represent a UserIdentity record."""
+try:
+    get_distribution('invenio-oauthclient')
+    from invenio_oauthclient import __version__ as OAUTHCLIENT_VERSION
 
-    __tablename__ = 'accounts_useridentity'
-
-    id = db.Column(db.String(255), primary_key=True, nullable=False)
-    method = db.Column(db.String(255), primary_key=True, nullable=False)
-    id_user = db.Column(db.Integer(), db.ForeignKey(User.id), nullable=False)
-
-    user = db.relationship(User, backref='external_identifiers')
-
-    __table_args__ = (db.Index(
-        'useridentity_id_user_method', id_user, method, unique=True), )
+    if parse_version(OAUTHCLIENT_VERSION) < parse_version('1.2.0'):
+        HAS_USER_IDENTITY = True
+except DistributionNotFound:
+    pass
 
 
-__all__ = ('UserIdentity', )
+if HAS_USER_IDENTITY:
+    from invenio_oauthclient.models import UserIdentity
+
+    __all__ = ()
+else:
+
+    class UserIdentity(db.Model, Timestamp):
+        """Represent a UserIdentity record."""
+
+        __tablename__ = 'accounts_useridentity'
+
+        id = db.Column(db.String(255), primary_key=True, nullable=False)
+        method = db.Column(db.String(255), primary_key=True, nullable=False)
+        id_user = db.Column(
+            db.Integer(), db.ForeignKey(User.id), nullable=False
+        )
+
+        user = db.relationship(User, backref='external_identifiers')
+
+        __table_args__ = (
+            db.Index(
+                'useridentity_id_user_method', id_user, method, unique=True
+            ),
+        )
+
+    __all__ = ('UserIdentity',)
