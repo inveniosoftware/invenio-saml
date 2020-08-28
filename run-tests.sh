19636c9bd9ad354dbdcb63ad6c07dead099dd3c6
KonstantinaStoikou <konstantinastoikou1@gmail.com>
2020-08-28 15:55:51 +0200

    global: fix isort, upgrade Python and dependencies

--- run-tests.sh
+++ run-tests.sh
@@ -7,7 +7,7 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 
 pydocstyle invenio_saml tests docs && \
-isort -rc -c -df && \
+isort invenio_saml tests --check-only --diff && \
 check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test
