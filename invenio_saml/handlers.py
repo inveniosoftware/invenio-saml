695058c79d84f89cfcf730d5a94da1a12d7e42bb
mb-wali <mb_wali@hotmail.com>
2021-05-25 08:51:29 +0200

    account_info: username set as external id

--- invenio_saml/handlers.py
+++ invenio_saml/handlers.py
@@ -46,11 +46,13 @@ def account_info(attributes, remote_app):
     surname = attributes[mappings['surname']][0]
     email = attributes[mappings['email']][0]
     external_id = attributes[mappings['external_id']][0]
+    username = external_id.split("@")[0] if "@" in external_id else external_id
 
     return dict(
         user=dict(
             email=email,
-            profile=dict(username=surname, full_name=name+" "+surname),
+            profile=dict(username=username,
+                         full_name=name+" "+surname),
         ),
         external_id=external_id,
         external_method=remote_app,
