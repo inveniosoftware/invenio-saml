bdec6351b13935d06347ed3bfd7546351c5a2ae1
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-05-25 23:32:25 +0200

    global: clean test infrastructure

--- run-tests.sh
+++ run-tests.sh
@@ -20,10 +20,9 @@ function cleanup() {
 trap cleanup EXIT
 
 
-python -m check_manifest --ignore ".*-requirements.txt"
+python -m check_manifest
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 eval "$(docker-services-cli up --db ${DB:-postgresql} --cache ${CACHE:-redis} --env)"
 python -m pytest
 tests_exit_code=$?
-python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
 exit "$tests_exit_code"
