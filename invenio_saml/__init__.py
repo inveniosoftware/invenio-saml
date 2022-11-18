6830935340d1d00470b3d59dc5662bcd738d2e61
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2022-11-18 10:04:06 +0100

    release: v1.0.0a3

--- invenio_saml/__init__.py
+++ invenio_saml/__init__.py
@@ -73,6 +73,6 @@ from __future__ import absolute_import, print_function
 
 from .ext import InvenioSAML
 
-__version__ = "1.0.0a2"
+__version__ = "1.0.0a3"
 
 __all__ = ("__version__", "InvenioSAML")
