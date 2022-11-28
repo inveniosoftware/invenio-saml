eac1566960264614216b6a50381d5975c241a882
AkshayUHegde <akshayudayhegde2@gmail.com>
2022-11-28 11:49:38 -0500

    Added OpenSearch to run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -23,7 +23,7 @@ trap cleanup EXIT
 python -m check_manifest
 python -m setup extract_messages --dry-run
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
-eval "$(docker-services-cli up --db ${DB:-postgresql} --cache ${CACHE:-redis} --env)"
+eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --cache ${CACHE:-redis} --env)"
 python -m pytest
 tests_exit_code=$?
 exit "$tests_exit_code"
