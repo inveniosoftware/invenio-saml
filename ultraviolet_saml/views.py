254ddb6b556989f4b2754372aa6d26d943f6b45c
AkshayUHegde <akshayudayhegde2@gmail.com>
2022-12-05 13:37:57 -0500

    Added SAML logout view

--- ultraviolet_saml/views.py
+++ ultraviolet_saml/views.py
@@ -7,7 +7,9 @@
 
 """Blueprint definitions."""
 
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for, current_app
+from flask_login import login_required
+from flask_security import logout_user
 
 blueprint = Blueprint(
     "ultraviolet_saml",
@@ -22,3 +24,10 @@ The sole purpose of this blueprint is to ensure that Invenio can find the
 templates and static files located in the folders of the same names next to
 this file.
 """
+
+
+@blueprint.route("/saml/logout")
+@login_required
+def logout_saml():
+    logout_user()
+    return redirect(url_for("sso_saml.slo", idp="nyu_mock"))
