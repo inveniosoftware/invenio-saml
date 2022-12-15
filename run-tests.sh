c073d30e0597c8af9e6b04fa6dbb37ba6e026c25
mojib <mojib.wali@tugraz.at>
2022-12-15 13:02:58 +0100

    ci: adds translation tests

--- run-tests.sh
+++ run-tests.sh
@@ -21,7 +21,6 @@ trap cleanup EXIT
 
 
 python -m check_manifest
-python -m setup extract_messages --dry-run
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 eval "$(docker-services-cli up --db ${DB:-postgresql} --cache ${CACHE:-redis} --env)"
 python -m pytest
